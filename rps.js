528ae16eb690c0b0284ded8152fbcdeaae77ccb7
practice24-7 <brian.d.hurst@gmail.com>
2021-05-05 06:57:26 -0700

    completed computerPlay function.  returns random rock, paper, or scissors

--- rps.js
+++ rps.js
@@ -5,12 +5,21 @@
 //step three is a function that will take the previous functions
 //and decide who wins
 
-function playRound(playerSelection, computerSelection) {
+//function playRound(playerSelection, computerSelection) {
     //code decides who wins
-}
+//}
 function computerPlay() {
     //randomizes computers turn (rock, paper, or scissors)
+    let computer = Math.floor(Math.random()*3);
+    
+    if (computer === 0) {
+        return "Rock"; 
+    } else if (computer === 1) {
+        return "Paper";
+    } else {
+        return "Scissors";
+    }
 }
-const playerSelection = "players input r/p/s";
-const computerSelection = computerPlay();
-console.log(playRound(playerSelection, computerSelection));
+//const playerSelection = "players input r/p/s";
+//const computerSelection = computerPlay();
+//console.log(playRound(playerSelection, computerSelection));
