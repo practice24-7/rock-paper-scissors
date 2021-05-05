89be22a5d1affdd21656c982f868236db66aa986
practice24-7 <brian.d.hurst@gmail.com>
2021-05-04 17:30:09 -0700

    added framework for javascript code (pseudocode)

---
+++ rps.js
@@ -0,0 +1,14 @@
+//this is the javascript of a rock paper scissors game.
+//step one is to create a function for the computer opponent.
+//this function will randomly choose either rock paper or scissors.
+//step two is to ask the user for their choice.
+//step three is a function that will take the previous functions
+//and decide who wins
+
+function playRound(playerSelection, computerSelection) {
+    //code
+}
+
+const playerSelection = "players input r/p/s";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
